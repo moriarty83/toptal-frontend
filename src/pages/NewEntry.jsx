3ee40d759768edafc7d55c9227d6f2a6a6e05a96
moriarty83 <moriarty83@gmail.com>
2022-02-28 20:20:57 -0500

    auto complete-ish

--- src/pages/NewEntry.jsx
+++ src/pages/NewEntry.jsx
@@ -18,9 +18,15 @@ function NewEntry({newFoodEntry}){
     /////////////
     // METHODS
     //////////////
-    const handleChange = (event) =>{
-        getSuggestion()
-        setFormData({...formData, [event.target.name]: event.target.value})
+    const  handleChange = async (event) =>{
+
+        if(event.target.name === 'product'){
+            getSuggestion(event)
+        }
+        else{
+            setFormData({...formData, [event.target.name]: event.target.value})        
+        }
+
     }
 
     const handleSubmit = (event) =>{
@@ -48,52 +54,59 @@ function NewEntry({newFoodEntry}){
         );
     }
 
-    const getSuggestion = ()=>{
-        return fetch("https://trackapi.nutritionix.com/v2/natural/nutrients",{
+    const getSuggestion = async (event)=>{
+        const key = event.target.name
+        const value = event.target.value
+
+        let suggestion = await fetch("https://trackapi.nutritionix.com/v2/natural/nutrients",{
             method: "post",
             headers: {
                 "x-app-id": state.appID,
                 "x-app-key": state.key,
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({query:formData.product}) 
+            body: JSON.stringify({query:value}) 
                
             })
             .then( (response) => {
-                if(response.ok){
-                    return response.json()
-                }
-                else{
-                    throw new Error("An error of type " + response.status + " occured")
-                };
+                console.log(response.status)
+                if(response.status === 200)
+                {return response.json()}
+                else return 
             })
-            .then((data)=>{
-                console.log(data)})
-            .catch((error) => {window.alert(error)}
-            );
+            // .then((data)=>{
+                
+
+            //     })
+            // .catch((error) => {window.alert(error)}
+        // );
+        console.log(suggestion)
+        suggestion ? setFormData({...formData, calories: suggestion.foods[0].nf_calories}) : setSuggestion()
+
     }
     return(
         <div className='card'>
             <h1>New Food Entry</h1>
             <form onSubmit={handleSubmit}>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">Food Name</span>
-                    <input onChange={handleChange} name="product" type="text" class="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Food Name</span>
+                    <input onChange={handleChange} name="product" value={formData.product} type="text" className="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
                 </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">Calories</span>
-                    <input onChange={handleChange} name="calories" type="number" class="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Calories</span>
+                    <input onChange={handleChange} name="calories" value={formData.calories} type="number" className="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
                 </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">Date</span>
-                    <input onChange={handleChange} name="date" type="date" class="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Date</span>
+                    <input onChange={handleChange} name="date" type="date" className="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
                 </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">Time</span>
-                    <input onChange={handleChange} name="time" type="time" class="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Time</span>
+                    <input onChange={handleChange} name="time" type="time" className="form-control" placeholder="New Food" aria-label="New Food" aria-describedby="basic-addon1" />
                 </div>
                 <input type="submit" />
             </form>
+            <p>{formData.calories}</p>
         </div>
     )
 }
